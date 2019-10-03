669c0c30f6ad4712aa6494da1734ef796b017528
Manpreet <manpreetkaurbajwa8968@gmail.com>
2019-10-02 20:18:28 -0700

    edit getpics.sh

--- bash/getpics.sh
+++ bash/getpics.sh
@@ -5,11 +5,10 @@ test -d ~/Pictures || mkdir ~/Pictures
 
 #Download the zip file of pictures to the Pictures directory
 test -f ~/Pictures/pics.zip || wget -q -0 ~/Pictures/pics.zip http://zonzorp.net/pics.zip
-test -f ~/Pictures/pics.tgz || wget -q -0 ~/Pictures/pics.zip http://zonzorp.net/pics.tgz
+test -f ~/Pictures/pics.tgz || wget -q -0 ~/Pictures/pics.tgz http://zonzorp.net/pics.tgz
 
 #unpack the downloaded files
 test -f ~/Pictures/pics.zip && unzip -d ~/Pictures -0 -q ~/Pictures/pics.zip && rm ~/Pictures/pics.zip
-test -f ~/Pictures/pics.tgz && unzip -d ~/Pictures -0 -q ~/Pictures/pics.tgz && rm ~/Pictures/pics.tgz
 
 #tarfile
 test -f ~/Pictures/pics.tgz && tar -xzf ~/Pictures/pics.tgz -C ~Pictures/&& rm ~/Pictures/pics.tgz
